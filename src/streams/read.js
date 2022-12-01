d82a78028b89618a0c02adf2522bb8e2647bf1f0
sesychev <sergeysychou@gmail.com>
2022-12-01 16:34:26 +0300

    feat: 7 task done

--- src/streams/read.js
+++ src/streams/read.js
@@ -22,24 +22,3 @@ const read = async () => {
 };
 
 await read();
-/*
-const fs = require("fs");
-const path = require("path");
-const process = require("process");
-
-const readPath = path.join(__dirname, "/text.txt");
-
-let readable = fs.createReadStream(readPath, {
-  encoding: "utf-8",
-});
-
-readable.pipe(process.stdout); //вывод текста в консоль
-
-const wtitePath = path.join(__dirname, "/temp.txt");
-
-let writable = fs.createWriteStream(wtitePath, {
-  encoding: "utf-8",
-});
-
-readable.pipe(writable); // запись в файл из потока/файла
-*/
