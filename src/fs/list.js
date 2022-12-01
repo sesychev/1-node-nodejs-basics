69206f75d8f32f0c986ebec3c8847d57461a89ae
sesychev <sergeysychou@gmail.com>
2022-12-01 16:18:07 +0300

    feat: 5 task done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import process from 'process';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const oldPath = path.join(__dirname, "files");
+
 const list = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+        // The check succeeded
+        await fs.promises.access(oldPath);
+        const files = await fs.promises.readdir(oldPath);
+        files.forEach((file) => process.stdout.write(file  + "\n"));
+    } catch (error) {
+        // The check failed
+        throw new Error("FS operation failed");
+    } 
 };
 
 await list();
