838133ebee9ac6ce4efccc0c6477f733f11cca73
sesychev <sergeysychou@gmail.com>
2022-12-01 18:31:37 +0300

    feat: 10 task done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import process from 'process';
+import { createHash } from 'crypto'
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const readPath = path.join(__dirname, "files", "/fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  // Write your code here
+  fs.readFile(readPath, (err, data) => {
+    if (err) console.log(err);
+    process.stdout.write(sha256(data) + '\n');
+  });
+
+  function sha256(content) {
+    return createHash('sha3-256').update(content).digest('hex')
+  }
 };
 
 await calculateHash();
