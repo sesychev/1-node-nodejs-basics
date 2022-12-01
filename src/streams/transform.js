fd8fb9f5a7e16d61045647f95332ba3c0f69c57d
sesychev <sergeysychou@gmail.com>
2022-12-01 17:55:24 +0300

    feat: 9 task done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,39 +1,22 @@
-import fs from "fs";
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { Transform, pipeline } from 'stream';
 import process from 'process';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const writePath = path.join(__dirname, "files", "/fileToWrite.txt");
-
-export const write = async () => {
-
-  const output = fs.createWriteStream(writePath);
-
-  process.stdin.on('data', (data) => {
-    output.write(data);
-  });
-  output.on('error', (error) => {
-    throw new Error(error);
-  });
-};
-
-write();
-/*
-import fs from "fs";
-import path from 'path';
-import { fileURLToPath } from 'url';
-import process from 'process';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const transform = async () => {
-    // Write your code here 
+// Write your code here
+  process.stdout.write("Please, write here something:" + "\n");
+  
+  const reverse = new Transform({
+      transform(chunk, encoding, callback) {
+        callback(null, reverseString(chunk));
+      },
+    });
+
+  function reverseString(str) {
+    return str.toString().split("").reverse().join("");
+}
+  pipeline(process.stdin, reverse, process.stdout, (error) => {
+    if (error) throw new Error(error);
+  });
 };
 
 await transform();
-*/
-
