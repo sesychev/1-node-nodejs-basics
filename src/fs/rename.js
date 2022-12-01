f8ff976e00a08ad53c4f94ce0f52a500726f30b9
sesychev <sergeysychou@gmail.com>
2022-12-01 14:44:57 +0300

    feat: 3 task done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const oldPath = path.join(__dirname, "files", "/wrongFilename.txt");
+const newPath = path.join(__dirname, "files", "/wrongFilename.md");
+
 const rename = async () => {
-    // Write your code here 
+    // Write your code here
+    try {
+       // The check succeeded
+        await fs.promises.access(oldPath);
+        fs.rename(oldPath, newPath, () => {
+            console.log("\nFile Renamed!\n");
+        });
+   } catch (error) {
+       // The check failed
+       throw new Error("FS operation failed");
+   }
 };
 
 await rename();
