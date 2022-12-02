7f554a401fde421d0cf8ae02980e463f39500b76
sesychev <sergeysychou@gmail.com>
2022-12-02 16:46:22 +0300

    feat: 15 task done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
-const parseArgs = () => {
-    // Write your code here 
+export const parseArgs = () => {
+// Write your code here
+  let arr = [];
+  const items = process.argv; //const items = ["--propName", "value"];
+  
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].startsWith('--')) {
+      let str = `${items[i].slice(2)} is ${items[i + 1]}`;
+      arr.push(str)
+    }
+  }
+  
+  console.log(arr.join(", "));
 };
 
 parseArgs();
