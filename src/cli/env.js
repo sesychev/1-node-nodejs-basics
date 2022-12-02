bcfcaaf253b85bfcaefec56fa0bb4c8b94b9046b
sesychev <sergeysychou@gmail.com>
2022-12-02 16:12:34 +0300

    feat: 14 task done

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
-const parseEnv = () => {
-    // Write your code here 
+export const parseEnv = () => {
+    // Write your code here
+    const prefix = 'RSS_';
+    let arr=[];
+    for (const item in process.env) {
+        let str = `${prefix}${item}=${process.env[item]}`;
+        arr.push(str); 
+    }
+
+    console.log(arr.join("; "));
 };
 
 parseEnv();
