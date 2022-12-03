84deb29f66a708edd442210940f8f08bf33e7d90
sesychev <sergeysychou@gmail.com>
2022-12-03 16:11:20 +0300

    feat: 16 task done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,15 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+import process from "process";
+import child_process from "child_process";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const readPath = path.join(__dirname, "files", "/script.js");
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  // Write your code here
 };
 
 spawnChildProcess();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,18 @@
-// n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { isMainThread, parentPort, workerData } from "worker_threads";
 
-const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+// n should be received from main thread
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+
+// This function sends result of nthFibonacci computations to main thread
+export const sendResult = () => {
+  if (isMainThread) {
+    // This code is executed in the main thread and not in the worker.
+    console.log("Execution in main thread");
+  } else {
+    // This code is executed in the worker and not in the main thread. Send a message to the main thread.
+    parentPort.postMessage(nthFibonacci(workerData));
+  }
 };
 
 sendResult();
