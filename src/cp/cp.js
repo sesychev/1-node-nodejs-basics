567713cfd624562ca1fa90913136ee77c7b77e13
sesychev <sergeysychou@gmail.com>
2022-12-04 15:57:33 +0300

    feat: 18, 19 & 20 tasks done

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,4 +1,3 @@
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 import process from "process";
@@ -6,10 +5,29 @@ import child_process from "child_process";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const readPath = path.join(__dirname, "files", "/script.js");
+const childPath = path.join(__dirname, "files", "/script.js");
 
-const spawnChildProcess = async (args) => {
+const args = process.argv.slice(2);
+
+export const spawnChildProcess = async (args) => {
   // Write your code here
+  let child = child_process.spawn("node", [childPath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on("data", (data) =>
+    console.log("Child data is indicated below... \n" + data)
+  );
+
+  child.on("spawn", () => console.log("Child process is here!\n"));
+
+  child.stderr.on("data", (data) => console.error("Stderr: " + data));
+
+  child.on("exit", (code) =>
+    console.log(`Child process exited with code ${code}`)
+  );
+
+  process.on("SIGINT", () => console.log(`Intercepting SIGINT`));
 };
 
-spawnChildProcess();
+spawnChildProcess(args);
