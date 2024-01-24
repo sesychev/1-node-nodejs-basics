62f147b0e8f821e39399c3604f67a17894220100
sesychev <sergeysychou@gmail.com>
2024-01-24 16:03:55 +0300

    streams: read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const pathFile = join(__dirname, "files/fileToRead.txt");
+  fs.access(pathFile, (err) => {
+    if (err) throw new Error(`FS operation failed`);
+    fs.createReadStream(pathFile, {
+      encoding: "utf-8",
+    }).pipe(process.stdout);
+  });
 };
 
 await read();
