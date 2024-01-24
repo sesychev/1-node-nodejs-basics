78e5462eac214f6a08da96d8f6a7565ab048e810
sesychev <sergeysychou@gmail.com>
2024-01-24 15:07:59 +0300

    fs: read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { join } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const pathFile = join(__dirname, "files/fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  fs.access(pathFile, (err) => {
+    if (err) throw new Error(`FS operation failed`);
+    const readStream = fs.createReadStream(pathFile, {
+      encoding: "utf-8",
+    });
+    readStream.on("data", (chunk) => {
+      console.log(chunk.toString());
+    });
+  });
 };
 
 await read();
