5980b657d783bd56d017b50db7026745144eafde
sesychev <sergeysychou@gmail.com>
2024-01-24 15:53:17 +0300

    hash: calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,26 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { join } from "path";
+import crypto from "crypto";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathFile = join(__dirname, "files/fileToCalculateHashFor.txt");
+  const hash = crypto.createHash("sha256").setEncoding("hex");
+  fs.access(pathFile, (err) => {
+    if (err) throw new Error(`FS operation failed`);
+    fs.createReadStream(pathFile, {
+      encoding: "utf-8",
+    })
+      .pipe(hash)
+      .on("finish", () => console.log(hash.read()))
+      .on("error", (err) => {
+        console.log(err.message);
+      });
+  });
 };
 
 await calculateHash();
