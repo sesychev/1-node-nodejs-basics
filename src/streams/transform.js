24e65b2e4add9b01a419b4926923b68d9e53398a
sesychev <sergeysychou@gmail.com>
2024-01-24 18:22:30 +0300

    streams: transform.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,18 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  //const callback = (str) => str.split("").reverse().join("");
+
+  const transform = new Transform({
+    transform(chunk, encoding, callback) {
+      chunk = String(chunk).split("").reverse().join("");
+      callback(null, chunk);
+    },
+  });
+
+  process.stdin.pipe(transform).pipe(process.stdout);
+  process.stdin.resume();
 };
 
 await transform();
+//streams: transform.js
