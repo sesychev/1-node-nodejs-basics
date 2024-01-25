591c5f0e1170d1a5b012ed599de3e5b48dcb4b62
sesychev <sergeysychou@gmail.com>
2024-01-25 16:01:05 +0300

    cli: env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
 const parseEnv = () => {
-    // Write your code here 
+  const obj = process.env;
+  let str = [];
+  Object.keys(process.env).forEach((key) => {
+    if (key.startsWith("RSS_")) str.push(`RSS_${key}=${obj[key]}`);
+  });
+  console.log(str.join("; "));
 };
 
 parseEnv();
